e4b5e9946312b14f605f115d8a3e39ab3478caea
Isis Silva <isis.silva@arctouch.com>
2022-09-27 23:30:02 -0300

    Initial Commit

---
+++ Garden/GardenApp.swift
@@ -0,0 +1,20 @@
+//
+//  GardenApp.swift
+//  Garden
+//
+//  Created by Isis Silva on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GardenApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
